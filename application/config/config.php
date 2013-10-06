e64c4736be47215870b45ecb67f7f4e467f3be97
anvoz <anvo4888@gmail.com>
2013-10-06 22:20:49 +0700

    Reset config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ciske/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
