15f905387f091022e5ba7d0756a5853bf77f4a1f
anvoz <anvo4888@gmail.com>
2014-01-11 01:36:02 +0700

    Group files with the same destination for better look

--- application/modules/addons/views/pagelet_files.php
+++ application/modules/addons/views/pagelet_files.php
@@ -1,5 +1,14 @@
 <ul>
-<?php foreach ($files as $file => $file_data): ?>
+<?php
+$last_dest = '';
+foreach ($files as $file => $file_data):
+    // Group files with the same destination for better look
+    if ( ! empty($last_dest) && $last_dest != $file_data['dest'])
+    {
+        echo '</ul><ul>';
+    }
+    $last_dest = $file_data['dest'];
+?>
     <li>
         <?php if ( ! empty($file_data['status'])): ?>
             <span class="label label-success">OK</span>
