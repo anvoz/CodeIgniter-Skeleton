82b4bdc418b5b782c5c94048f0108d444471769a
anvoz <anvo4888@gmail.com>
2014-01-11 13:21:16 +0700

    Add Ion auth's language files to addon config

--- application/modules/addons/config/addon_ion_auth.php
+++ application/modules/addons/config/addon_ion_auth.php
@@ -24,6 +24,8 @@ $config['files'] = array(
     'config/ion_auth.php' => array('dest' => 'application/config'),
     'controllers/auth.php' => array('dest' => 'application/modules/auth/controllers'),
     'controllers/auth_ajax.php' => array('dest' => 'application/modules/auth/controllers'),
+    'language/auth_lang.php' => array('dest' => 'application/language/english'),
+    'language/ion_auth_lang.php' => array('dest' => 'application/language/english'),
     'libraries/Ion_auth/Ion_auth.php' => array('dest' => 'application/libraries/Ion_auth'),
     'libraries/Authentication.php' => array('dest' => 'application/libraries'),
     'models/ion_auth_model.php' => array('dest' => 'application/models'),
