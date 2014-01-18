d314442154dd9cef8dfbf65c8cae314435882c58
anvoz <anvo4888@gmail.com>
2014-01-18 10:25:21 +0700

    Change add-ons layout Put all installation sections to collapsed panel Edit wording

--- application/modules/addons/views/pagelet_addon.php
+++ application/modules/addons/views/pagelet_addon.php
@@ -1,42 +1,86 @@
+<?php $addon_id = str_replace('_', '-', $addon_key); ?>
+
 <?php ( ! empty($addon['description'])) && print($addon['description']); ?>
 
 <h3>Installation</h3>
+<div id="<?php echo $addon_id; ?>-installation" class="panel-group">
 <?php if ( ! empty($addon['require'])): ?>
-    <h4>Require (manually copy / delete)</h4>
-    <ul>
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h4 class="panel-title">
+                <a class="accordion-toggle" href="#<?php echo $addon_id; ?>-require"
+                    data-toggle="collapse" data-parent="#<?php echo $addon_id; ?>-installation"
+                >
+                    <span class="caret"></span>
+                    Require: manually install / uninstall required add-on(s)
+                </a>
+            </h4>
+        </div>
+        <ul id="<?php echo $addon_id; ?>-require" class="list-group panel-collapse collapse in">
         <?php foreach ($addon['require'] as $require): ?>
-            <li><a href="#<?php echo $require; ?>">#<?php echo $require; ?></a></li>
+            <li class="list-group-item">
+                <a href="#<?php echo $require; ?>">#<?php echo $require; ?></a>
+            </li>
         <?php endforeach; ?>
-    </ul>
+        </ul>
+    </div>
 <?php endif; ?>
 
 <?php if ( ! empty($pagelet_copy_files)): ?>
-    <h4>Auto (copy & delete)</h4>
-    <?php echo $pagelet_copy_files; ?>
-    <p>
-        <a class="btn btn-primary" href="#"
-            rel="async" ajaxify="<?php echo site_url('addons/addons_ajax/action/copy/' . $addon_key); ?>"
-        >Copy</a>
-        <a class="btn btn-danger" href="#"
-            rel="async" ajaxify="<?php echo site_url('addons/addons_ajax/action/delete/' . $addon_key); ?>"
-        >Delete</a>
-        <?php if ( ! empty($addon['require'])): ?>
-            <p><i>Don't forget to add / remove addon(s) in the Require section above.</i></p>
-        <?php endif; ?>
-    </p>
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <div class="pull-right" style="margin-top: -2px;">
+                <a class="btn btn-primary btn-xs" href="#"
+                    rel="async" ajaxify="<?php echo site_url('addons/addons_ajax/action/copy/' . $addon_key); ?>"
+                    <?php if ( ! empty($addon['require'])): ?>
+                        data-toggle="tooltip" data-placement="top" data-original-title="Don't forget to install addon(s) in the Require section yourself"
+                    <?php endif; ?>
+                >Copy</a>
+                <a class="btn btn-danger btn-xs" href="#"
+                    rel="async" ajaxify="<?php echo site_url('addons/addons_ajax/action/delete/' . $addon_key); ?>"
+                    <?php if ( ! empty($addon['require'])): ?>
+                        data-toggle="tooltip" data-placement="top" data-original-title="Don't forget to uninstall addon(s) in the Require section yourself"
+                    <?php endif; ?>
+                >Delete</a>
+            </div>
+            <h4 class="panel-title">
+                <a class="accordion-toggle" href="#<?php echo $addon_id; ?>-auto"
+                    data-toggle="collapse" data-parent="#<?php echo $addon_id; ?>-installation"
+                >
+                    <span class="caret"></span>
+                    Auto: copy / delete file(s)
+                </a>
+            </h4>
+        </div>
+        <div id="<?php echo $addon_id; ?>-auto" class="list-group panel-collapse collapse">
+            <?php echo $pagelet_copy_files; ?>
+        </div>
+    </div>
 <?php endif; ?>
 
 <?php if ( ! empty($addon['manual'])): ?>
-    <h4>Manual</h4>
-    <ul>
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h4 class="panel-title">
+                <a class="accordion-toggle" href="#<?php echo $addon_id; ?>-manual"
+                    data-toggle="collapse" data-parent="#<?php echo $addon_id; ?>-installation"
+                >
+                    <span class="caret"></span>
+                    Manual
+                </a>
+            </h4>
+        </div>
+        <ul id="<?php echo $addon_id; ?>-manual" class="list-group panel-collapse collapse">
         <?php foreach ($addon['manual'] as $manual): ?>
-            <li><?php echo $manual; ?></li>
+            <li class="list-group-item"><?php echo $manual; ?></li>
         <?php endforeach; ?>
-    </ul>
+        </ul>
+    </div>
 <?php endif; ?>
+<div>
 
 <?php if ( ! empty($addon['skeleton'])): ?>
-    <h3>Skeleton (only available after copied)</h3>
+    <h3>Skeleton (only available after installed)</h3>
 
     <p>
         <a target="_blank" href="<?php echo site_url('#' . str_replace('_', '-', $addon_key)); ?>">
--- application/modules/addons/views/pagelet_files.php
+++ application/modules/addons/views/pagelet_files.php
@@ -1,21 +1,22 @@
-<ul>
+<div class="list-group-item">
 <?php
 $last_dest = '';
 foreach ($files as $file => $file_data):
     // Group files with the same destination for better look
     if ( ! empty($last_dest) && $last_dest != $file_data['dest'])
     {
-        echo '</ul><ul>';
+        echo '</div><div class="list-group-item">';
     }
     $last_dest = $file_data['dest'];
 ?>
-    <li>
+    <div>
+        &bull;
         <?php if ( ! empty($file_data['status'])): ?>
             <span class="label label-success">OK</span>
         <?php elseif (isset($file_data['status'])): ?>
             <span class="label label-danger">Failed</span>
         <?php endif; ?>
         <?php echo $file . ' &rarr; ' . $file_data['dest'] . '/' . $file; ?>
-    </li>
+    </div>
 <?php endforeach; ?>
-</ul>
+</div>
