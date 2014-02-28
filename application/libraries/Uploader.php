50d56f231e901301d7eb959d151128eb74ac9d0d
anvoz <anvo4888@gmail.com>
2014-02-28 18:21:29 +0700

    Install fileupload add-on

---
+++ application/libraries/Uploader.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/jQuery_file_upload/Uploadhandler.php';
+
+class Uploader extends Uploadhandler {}
+
+/* End of file Uploader.php */
+/* Location: ./application/libraries/Uploader.php */
