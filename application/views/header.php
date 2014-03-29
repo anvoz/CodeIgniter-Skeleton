ea63bb838e2504b185dbca466f56cba69356df6e
anvoz <anvo4888@gmail.com>
2014-03-29 23:44:47 +0700

    Add todo example to header

--- application/views/header.php
+++ application/views/header.php
@@ -12,6 +12,14 @@
             <ul class="nav navbar-nav">
                 <li><a href="<?php echo site_url(); ?>">Home</a></li>
                 <li><a href="<?php echo site_url('addons'); ?>">Add-ons</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        Example <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="<?php echo site_url('todo'); ?>">Todo</a></li>
+                    </ul>
+                </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a target="_blank" href="https://github.com/anvoz/CodeIgniter-Skeleton">Github</a></li>
