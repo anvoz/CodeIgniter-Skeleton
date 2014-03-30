53026b3149e0f8466a8ce167a87d8b0cffb39bca
anvoz <anvo4888@gmail.com>
2014-03-30 12:19:41 +0700

    Fix typo

--- application/modules/todo/views/index.php
+++ application/modules/todo/views/index.php
@@ -3,7 +3,7 @@
 <?php echo $pagelet_todo; ?>
 
 <?php
-// As it was designed to be an independent components,
+// As it was designed to be an independent component,
 // you can try to use the todo pagelet anywhere.
 // echo '<hr>';
 // echo Modules::run('todo/_pagelet_todo');
